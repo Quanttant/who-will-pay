c56b5855f85a0426a1a9d547718e13fddf19cc03
Furkan Kaya <8fkaya@gmail.com>
2021-03-11 19:39:04 +0300

    Basic select mechanic done.

--- Assets/DetectTouch.cs
+++ Assets/DetectTouch.cs
@@ -14,6 +14,8 @@ public class DetectTouch : MonoBehaviour
 
     private ColorManager _colorManager;
 
+    bool selectedOne = false;
+
     private void Start()
     {
         _colorManager = FindObjectOfType<ColorManager>();
@@ -21,57 +23,60 @@ public class DetectTouch : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { 
+    {
+        if (Input.touchCount == 0)
+        {
+            if (selectedOne)
+            {
+                selectedOne = false;
+                Camera.main.backgroundColor = _colorManager.backgroundColor;
+            }
+        }
+
+        if (2 <= Input.touchCount)
+        {
+            counter -= Time.deltaTime;
+            if (counter <= 0 && !selectedOne)
+            {
+                selectedOne = true;
+                int randomIndex = Random.Range(0, Input.touchCount);
+                Touch selectedTouch = touches[randomIndex];
+                foreach (Touch touch in touches)
+                    if (touch != selectedTouch) touch.Lost();
+                    else touch.Won();
+            }
+        }
         int i = 0;
         while (i < Input.touchCount)
         {
-            if (2 <= Input.touchCount)
-            {
-                counter -= Time.deltaTime;
-                if (counter <= 0)
-                {
-                    int randomIndex = Random.Range(0, Input.touchCount);
-                    Touch selectedTouch = touches[randomIndex];
-                    Camera.main.backgroundColor = selectedTouch.color;
-                    touches.ForEach(x => Destroy(x.gameObject));
-                }
-            }
             UnityEngine.Touch t = Input.GetTouch(i);
             if (t.phase == TouchPhase.Began)
             {
-                Debug.Log("touch began");
-                touches.Add(createCircle(t));
+                createCircle(t);
                 counter = maxTime;
             }
             else if (t.phase == TouchPhase.Ended)
             {
-                Touch thisTouch = touches.Find(x => x.fingerId == t.fingerId);
-                _colorManager.colors.Add(thisTouch.color);
-                Destroy(thisTouch.gameObject);
-                touches.RemoveAt(touches.IndexOf(thisTouch));
                 counter = maxTime;
+                Touch thisTouch = touches.Find(x => x.fingerId == t.fingerId);
+                Destroy(thisTouch.gameObject);
             }
             else if (t.phase == TouchPhase.Moved)
             {
                 Touch thisTouch = touches.Find(x => x.fingerId == t.fingerId);
                 thisTouch.GetComponent<RectTransform>().anchoredPosition = ConvertToCanvasPos(t.position);
             }
-            ++i;
+            i++;
         }
     }
 
-    Touch createCircle(UnityEngine.Touch t)
+    void createCircle(UnityEngine.Touch t)
     {
         GameObject c = Instantiate(circle, transform) as GameObject;
-        c.name = "Touch" + t.fingerId;
+        c.name = "Touch_" + t.fingerId;
         c.GetComponent<RectTransform>().anchoredPosition = ConvertToCanvasPos(t.position);
         Touch touchScript = c.GetComponent<Touch>();
-        //c.transform.Find("Center").GetComponent<SVGImage>().color = _colorManager.colors[0];
-        c.transform.Find("Center").GetComponent<Image>().color = _colorManager.colors[0];
         touchScript.fingerId = t.fingerId;
-        touchScript.color = _colorManager.colors[0];
-        _colorManager.colors.RemoveAt(0);
-        return c.GetComponent<Touch>();
     }
 
 
--- Assets/Touch.cs
+++ Assets/Touch.cs
@@ -7,8 +7,36 @@ public class Touch : MonoBehaviour
 {
     public int fingerId;
     public Color color;
+
+    private ColorManager _colorManager;
+    private DetectTouch _touchManager;
+
+    private void Start()
+    {
+        _colorManager = FindObjectOfType<ColorManager>();
+        _touchManager = FindObjectOfType<DetectTouch>();
+        _touchManager.touches.Add(this);
+
+        //transform.Find("Center").GetComponent<SVGImage>().color = _colorManager.colors[0];
+        color = _colorManager.colors[0];
+        transform.Find("Center").GetComponent<Image>().color = color;
+        _colorManager.colors.RemoveAt(0);
+    }
+
+    public void Won()
+    {
+        Camera.main.backgroundColor = color;
+        transform.Find("Background").gameObject.SetActive(true);
+    }
+
+    public void Lost()
+    {
+        transform.Find("Center").GetComponent<Image>().enabled = false;
+    }
+
     private void OnDestroy()
     {
-        
+        _colorManager.colors.Add(color);
+        _touchManager.touches.Remove(this);
     }
 }
